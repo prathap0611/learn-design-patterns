cbbfe1da23f86aa1fd8ec6995f4311e6e8e977a4
Prathap_Hari <hari@solitontech.com>
2016-02-07 13:58:15 +0530

    Added cat list and display selected cat

--- app/controller/dashboard.controller.js
+++ app/controller/dashboard.controller.js
@@ -11,10 +11,31 @@ mainApp.controller('dashboard', ['$scope', function($scope) {
             name: "Cat 2",
             image: "../../assets/cat2.jpg",
             clickCount:0
+        },
+        {
+            name: "Cat 3",
+            image: "../../assets/cat3.jpg",
+            clickCount:0
+        },
+        {
+            name: "Cat 4",
+            image: "../../assets/cat4.jpg",
+            clickCount:0
+        },
+        {
+            name: "Cat 5",
+            image: "../../assets/cat5.jpg",
+            clickCount:0
         }
-    ]
+    ];
     
-    $scope.incrementCount = function(index) {
-        $scope.catDetails[index].clickCount++;
+    $scope.selectedCatIndex = 0;
+    
+    $scope.showCat = function(index) {
+        $scope.selectedCatIndex = index; 
+    };
+    
+    $scope.incrementCount = function() {
+        $scope.catDetails[$scope.selectedCatIndex].clickCount++;
     };
 }]);
