a00359e6aab1a066dbb67710fbe87f5b9236d4ec
Prathap_Hari <hari@solitontech.com>
2016-02-07 12:19:29 +0530

    Updated to support two cats

--- app/controller/dashboard.controller.js
+++ app/controller/dashboard.controller.js
@@ -1,8 +1,20 @@
 var mainApp = angular.module('mainApp', []);
 
 mainApp.controller('dashboard', ['$scope', function($scope) {
-    $scope.clickCount = 0;
-    $scope.incrementCount = function() {
-        $scope.clickCount ++;
+    $scope.catDetails = [
+        {
+            name: "Cat 1",
+            image: "../../assets/cat1.jpg",
+            clickCount:0
+        },
+        {
+            name: "Cat 2",
+            image: "../../assets/cat2.jpg",
+            clickCount:0
+        }
+    ]
+    
+    $scope.incrementCount = function(index) {
+        $scope.catDetails[index].clickCount++;
     };
 }]);
